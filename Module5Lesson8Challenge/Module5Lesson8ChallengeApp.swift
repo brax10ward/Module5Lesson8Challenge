8c47cf6b2819d193388f00216099b8e281e25f04
Braxton Ward <bward@grio.com>
2022-09-16 14:07:20 -0800

    Initial Commit

---
+++ Module5Lesson8Challenge/Module5Lesson8ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module5Lesson8ChallengeApp.swift
+//  Module5Lesson8Challenge
+//
+//  Created by Braxton Ward on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Module5Lesson8ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
